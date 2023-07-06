cb3fe281560bac0e4f776db7c31d9d13d032384e
matsan-1219 <sandy_9011@live.com.mx>
2023-07-06 17:36:14 -0600

    primer practica de git

---
+++ multiHerencia-interfaces_SAG/src/common/OperacionesCientificas.java
@@ -0,0 +1,11 @@
+package common;
+
+public interface OperacionesCientificas {
+
+	
+	
+	double seno(double angulo);
+	double potencia(double base, int exponente);
+	double raiz(double numero);
+	
+}
