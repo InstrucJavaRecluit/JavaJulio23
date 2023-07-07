624d2652165449e2c4fdfe557b0313f9bbcd9795
RosiCantu <RosiCantu>
2023-07-07 17:14:08 -0600

    mi primera practica de git

---
+++ multiherencia-interfaces_RLIC/src/common/CalcBasica.java
@@ -0,0 +1,24 @@
+package common;
+
+public class CalcBasica extends Calculadora{
+	
+	public CalcBasica() {
+		
+	}
+	
+	//como hacer eso
+
+	@Override
+	public double sumar(int a, double b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+	@Override
+	public double restar(int a, double b) {
+		// TODO Auto-generated method stub
+		return a - b;
+		
+	}
+
+}
